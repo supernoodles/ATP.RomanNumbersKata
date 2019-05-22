15829e57454b2042fb16b9a7a42caa739a76ffa6
Shorter, Graeme <Graeme.Shorter@mandg.com>
2019-05-22 15:01:36 +0100

    GivenArabicNumberOutsideRomanRange_ThrowArgumentException

--- ATP.RomanNumbersKata.Source/RomanNumeralConverter.cs
+++ ATP.RomanNumbersKata.Source/RomanNumeralConverter.cs
@@ -1,9 +1,16 @@
-﻿namespace ATP.RomanNumbersKata.Source
+﻿
+namespace ATP.RomanNumbersKata.Source
 {
+    using System;
     public class RomanNumeralConverter
     {
         public string ToRoman(int arabic)
         {
+            if (arabic <= 0 || arabic >= 4000)
+            {
+                throw  new ArgumentException();
+            }
+
             var romanNumerals = new[] {"I", "II", "III", "IV"};
 
             return romanNumerals[arabic - 1];
--- ATP.RomanNumbersKata.Tests/RomanNumeralConverterShould.cs
+++ ATP.RomanNumbersKata.Tests/RomanNumeralConverterShould.cs
@@ -1,5 +1,6 @@
 ﻿namespace ATP.RomanNumbersKata.Tests
 {
+    using System;
     using NUnit.Framework;
     using Source;
 
@@ -14,10 +15,19 @@
             _romanNumeralConverter = new RomanNumeralConverter();
         }
 
+        [TestCase(4000)]
+        [TestCase(4030)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void GivenArabicNumberOutsideRomanRange_ThrowArgumentException(int arabic)
+        {
+            Assert.That(() =>_romanNumeralConverter.ToRoman(arabic), Throws.TypeOf<ArgumentException>());
+        }
+
         [TestCase(1, ExpectedResult = "I")]
         [TestCase(2, ExpectedResult = "II")]
         [TestCase(4, ExpectedResult = "IV")]
-        public string GivenArabicNumber_ReturnExpectedRomanNumeral(int arabic) =>
+        public string GivenArabicNumberInsideRomanRange_ReturnExpectedRomanNumeral(int arabic) =>
             _romanNumeralConverter.ToRoman(arabic);
     }
 }
