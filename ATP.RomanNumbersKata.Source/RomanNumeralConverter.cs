cb7d4ff0ef9c7a81cd22989b9020f796edb7a003
Steve Cusack <stephen.cusack@mandg.com>
2019-05-24 10:10:52 +0100

    refactor

--- ATP.RomanNumbersKata.Source/RomanNumeralConverter.cs
+++ ATP.RomanNumbersKata.Source/RomanNumeralConverter.cs
@@ -1,23 +1,33 @@
 ﻿namespace ATP.RomanNumbersKata.Source
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class RomanNumeralConverter
     {
         private readonly SortedDictionary<int,string> _romanNumeralsLookup = new SortedDictionary<int, string>
         {
             {1, "I"},
-            {2, "II"},
-            {3, "III"},
             {4, "IV"},
             {5, "V"},
             {9, "IX"},
-            {10, "X"}
+            {10, "X"},
         };
 
         public string ToRoman(int arabic)
         {
-            return _romanNumeralsLookup[arabic];
+            var result = "";
+
+            foreach (var number in _romanNumeralsLookup.Keys.Reverse())
+            {
+                while (arabic >= number)
+                {
+                    result += _romanNumeralsLookup[number];
+                    arabic -= number;
+                }
+            }
+
+            return result;
         }
     }
 }
